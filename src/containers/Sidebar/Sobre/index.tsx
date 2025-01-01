8d3caca4ae85b0928f0241ed54c6fc7d3d773098
Augusto Cesar O Nunes <augustocz.dev@gmail.com>
2025-01-01 17:48:27 -0400

    construindo a estrutura e a secao "Sobre mim"

---
+++ src/containers/Sidebar/Sobre/index.tsx
@@ -0,0 +1,19 @@
+import Titulo from "../../../components/Titulo"
+import Paragrafo from "../../../components/Paragrafo"
+import { GithubSecao } from "./styles"
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur officiis
+      nobis eligendi eveniet nihil voluptas, assumenda qui, officia ab earum ad?
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=Augusto-C2304&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=Augusto-C2304&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from "../../components/Titulo"
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/Augusto-C2304.png" />
+    <Titulo fontSize={Number(20)}>Augusto Cesar</Titulo>
+  </aside>
+)
+
+export default Sidebar
